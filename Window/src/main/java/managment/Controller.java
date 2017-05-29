7f26876b9508117edb94f0e4b26b5c1bf6877fa8
Karoru19 <budlewskikarol@gmail.com>
2017-05-29 16:08:06 +0200

    Initialize project

---
+++ Window/src/main/java/managment/Controller.java
@@ -0,0 +1,7 @@
+package managment;
+
+/**
+ * Created by karoru on 16.05.17.
+ */
+public class Controller {
+}
---
+++ Window/src/main/java/signup/Controiller.java
@@ -0,0 +1,7 @@
+package signup;
+
+/**
+ * Created by karoru on 23.05.17.
+ */
+public class Controiller {
+}
