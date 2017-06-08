6e89142d9a74bbfc0f5a8a85a482e282c5a7683b
Arquanite <krynkaxd@gmail.com>
2017-06-08 11:03:34 +0200

    never egen

--- client-ejb/src/main/java/ejb/UserDao.java
+++ client-ejb/src/main/java/ejb/UserDao.java
@@ -4,6 +4,7 @@ import model.User;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import java.util.List;
 
 /**
  * Created by karoru on 02.06.17.
@@ -12,6 +13,14 @@ import javax.persistence.EntityManager;
 public class UserDao {
     private EntityManager em;
 
-    public void save(User user) { em.persist(user); }
+    public void add(User user) { em.persist(user); }
     public void remove(Long id) { em.remove(em.getReference(User.class, id)); }
+    public void update(User user) { }
+
+    public boolean authenticate(String login, String passwd){ /* niewiem xd */ }
+    public boolean isAdmin(String login){ }
+
+    public List<User> findAll() { }
+
+
 }
