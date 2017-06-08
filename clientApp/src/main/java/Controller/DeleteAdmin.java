c01905772ae1dbf5bc38a985ec0f22ef3385bef7
dzieszk <tobi.dzieszk@gmail.com>
2017-06-08 23:47:40 +0200

    raaak

---
+++ clientApp/src/main/java/Controller/DeleteAdmin.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by dzieszk on 08.06.17.
+ */
+public class DeleteAdmin {
+}
---
+++ clientApp/src/main/java/Controller/DeleteBook.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by dzieszk on 08.06.17.
+ */
+public class DeleteBook {
+
+}
