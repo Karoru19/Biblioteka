d548ee0992e94ec3f3ca8640a96d035f63d2892f
dzieszk <tobi.dzieszk@gmail.com>
2017-06-08 11:04:02 +0200

    Y u do dis to me?

--- client-ejb/src/main/java/ejb/RentalDao.java
+++ client-ejb/src/main/java/ejb/RentalDao.java
@@ -1,9 +1,13 @@
 package ejb;
 
+import model.Book;
 import model.Rental;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by karoru on 02.06.17.
@@ -13,5 +17,29 @@ public class RentalDao {
     private EntityManager em;
 
     public void save(Rental rental) { em.persist(rental); }
+
+    public List<Rental> findAll() {
+        TypedQuery<Rental> query = em.createNamedQuery(Rental.findAll, Rental.class);
+        return query.getResultList();
+    }
+
+    public List<Rental> findByBook(Book book) {
+        TypedQuery<Rental> query = em.createNamedQuery(Rental.findByBook, Rental.class);
+        query.setParameter(1, book);
+        return query.getResultList();
+    }
+
+    public List<Rental> findByRentDate(Date date) {
+        TypedQuery<Rental> query = em.createNamedQuery(Rental.findByRentDate, Rental.class);
+        query.setParameter(1, date);
+        return query.getResultList();
+    }
+
+    public List<Rental> findByReturnDate(Date date) {
+        TypedQuery<Rental> query = em.createNamedQuery(Rental.findByReturnDate, Rental.class);
+        query.setParameter(1, date);
+        return query.getResultList();
+    }
+
     public void remove(Long id) { em.remove(em.getReference(Rental.class, id)); }
 }
--- client-service/src/main/java/ejb/RentalBeanRemote.java
+++ client-service/src/main/java/ejb/RentalBeanRemote.java
@@ -1,7 +1,22 @@
 package ejb;
 
+import model.Book;
+import model.Rental;
+
+import javax.ejb.Remote;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by karoru on 02.06.17.
  */
+@Remote
 public interface RentalBeanRemote {
+    public void save(Rental rental);
+    public List<Rental> findAll();
+    public List<Rental> findByBook(Book book);
+    public List<Rental> findByRentDate(Date date);
+    public List<Rental> findByReturnDate(Date date);
+    public void remove(Long id);
 }
